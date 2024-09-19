e829098f29e7387bcb823ee4ffeceaa99b1c819f
rithika2035 <rithikapamu05@gmail.com>
2024-09-19 19:21:00 +0530

    final commit-1

--- frontend/src/components/ListItems.jsx
+++ frontend/src/components/ListItems.jsx
@@ -3,14 +3,19 @@ import { ShopContext } from '../Context/ShopContext'
 import {TbTrash} from "react-icons/tb"
 import { useNavigate } from "react-router-dom"
 
-const WishlistItems = () => {
+const ListItems = () => {
   const navigate = useNavigate();
-  const{ all_products, listItems, removeFromList, url} = useContext(ShopContext);
+  const{getTotalCartAmount, all_products, cartItems, removeFromCart, url} = useContext(ShopContext);
+  // Function to navigate to the product page
+  const handleProductClick = (productId) => {
+    navigate(`/product/${productId}`);  // Redirect to the product page based on the product ID
+  };
   return (
     <section className='p-20 mt-10 pt-28'>
-      <table className='w-full mx-auto'> 
-        <thead>
-          <tr className='bg-slate-900/10 regular-18 sm:regular-22 text-start'>
+            <table className='w-full mx-auto'> 
+                <thead>
+                    <tr className='bg-slate-900/10 regular-18 sm:regular-22
+                    text-start'>
                         <th className='p-1 py-2'>Product</th>
                         <th className='p-1 py-2'>Title</th>
                         <th className='p-1 py-2'>Price</th>
@@ -21,15 +26,15 @@ const WishlistItems = () => {
                 </thead>
                 <tbody>
                     {all_products.map((product) => {
-                        if (listItems[product._id] > 0) {
+                        if (cartItems[product._id] > 0) {
                             return <tr key={product._id} className='border-b border-slate-900/20 p-6 medium-14 text-center'>
-                                <td className='flexCenter'><img src={url+"/images/"+product.image} alt="prdctImg" height={43} width={43} className='rounded-lg rign-1 ring-slate-900/5 my-1'/></td>
-                                <td><div className='line-clamp-3'>{product.name}</div></td>
+                                <td className='flexCenter'><img src={url+"/images/"+product.image} alt="prdctImg" height={43} width={43} className='rounded-lg rign-1 ring-slate-900/5 my-1 cursor-pointer' onClick={() => handleProductClick(product._id)}/></td>
+                                <td><div className='line-clamp-3 cursor-pointer' onClick={() => handleProductClick(product._id)}>{product.name}</div></td>
                                 <td>₹ {product.price}.00</td>
-                                <td className='w-16 h-16 bg-white'>{listItems[product._id]}</td>
-                                <td>₹ {product.price * listItems[product._id]}</td>
+                                <td className='w-16 h-16 bg-white'>{cartItems[product._id]}</td>
+                                <td>₹ {product.price * cartItems[product._id]}</td>
                                 <td>
-                                    <div className='bold-22 pl-14 '><TbTrash onClick={()=> removeFromList(product._id)}/></div>
+                                    <div className='bold-22 pl-14 '><TbTrash onClick={()=> removeFromCart(product._id)}/></div>
                                 </td>
 
                             </tr>
@@ -43,4 +48,4 @@ const WishlistItems = () => {
   )
 }
 
-export default WishlistItems
+export default ListItems
