541ef61eb7e3c2887939272ad96ea6a638374f64
rithika2035 <rithikapamu05@gmail.com>
2024-09-19 20:01:10 +0530

    final commit-2

--- frontend/src/components/ListItems.jsx
+++ frontend/src/components/ListItems.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom"
 
 const ListItems = () => {
   const navigate = useNavigate();
-  const{getTotalCartAmount, all_products, cartItems, removeFromCart, url} = useContext(ShopContext);
+  const{getTotalCartAmount, all_products, listItems, removeFromList, url} = useContext(ShopContext);
   // Function to navigate to the product page
   const handleProductClick = (productId) => {
     navigate(`/product/${productId}`);  // Redirect to the product page based on the product ID
@@ -26,15 +26,15 @@ const ListItems = () => {
                 </thead>
                 <tbody>
                     {all_products.map((product) => {
-                        if (cartItems[product._id] > 0) {
+                        if (listItems[product._id] > 0) {
                             return <tr key={product._id} className='border-b border-slate-900/20 p-6 medium-14 text-center'>
                                 <td className='flexCenter'><img src={url+"/images/"+product.image} alt="prdctImg" height={43} width={43} className='rounded-lg rign-1 ring-slate-900/5 my-1 cursor-pointer' onClick={() => handleProductClick(product._id)}/></td>
                                 <td><div className='line-clamp-3 cursor-pointer' onClick={() => handleProductClick(product._id)}>{product.name}</div></td>
                                 <td>₹ {product.price}.00</td>
-                                <td className='w-16 h-16 bg-white'>{cartItems[product._id]}</td>
-                                <td>₹ {product.price * cartItems[product._id]}</td>
+                                <td className='w-16 h-16 bg-white'>{listItems[product._id]}</td>
+                                <td>₹ {product.price * listItems[product._id]}</td>
                                 <td>
-                                    <div className='bold-22 pl-14 '><TbTrash onClick={()=> removeFromCart(product._id)}/></div>
+                                    <div className='bold-22 pl-14 '><TbTrash onClick={()=> removeFromList(product._id)}/></div>
                                 </td>
 
                             </tr>
